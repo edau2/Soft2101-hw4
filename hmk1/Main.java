e27ac1721a5c99aef5c4f1a2d434b41a88aeae66
Eda Ulker <22comp1006@isik.edu.tr>
2026-01-01 21:49:57 +0300

    satisfying requirement change two

--- hmk1/Main.java
+++ hmk1/Main.java
@@ -1,24 +1,6 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter first number:");
-        int num1 = scanner.nextInt();
-
-        System.out.println("Enter second number:");
-        int num2 = scanner.nextInt();
-
-        System.out.println("Numbers between " + num1 + " and " + num2 + ":");
-
-        // Kucukten buyuge siralamak icin kontrol
-        int start = Math.min(num1, num2);
-        int end = Math.max(num1, num2);
-
-        for (int i = start + 1; i < end; i++) {
-            System.out.print(i + " ");
+for (int i = start + 1; i < end; i++) {
+            if (i % 2 != 0) {
+                 System.out.print(i + " ");
+            }
         }
-        System.out.println();
-    }
-}
+
